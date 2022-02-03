bd8b3e238ef947433b2e6c3c2fbde61ca481f804
Kevin Vermaat <kevinvermaat@icloud.com>
2022-02-03 10:01:37 +0100

    wat we in de les hebben gedaan aan de code

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from random import randrange as rnd
 from itertools import cycle
 from random import choice
 import pygame
+import texturer
 
 pygame.init()
 speed = 1
 
-
+#displays game window
+displayGame = pygame.display.set_mode((600, 300))
+clock = pygame.time.Clock()
+playerState = player_init
--- texturer.py
+++ texturer.py
@@ -1,1 +1,30 @@
-print("test=true")
+from PIL import Image
+
+def set_textures(filename):
+    global player_init
+    player_init = Image.open("{filename}\player.png".format(filename=filename)).convert("RGBA") #player start position
+    global player_frame_1
+    global player_frame_2
+    global player_frame_3
+    global player_frame_4
+    #player_frame_1
+    #player_frame_2
+    #player_frame_3
+    #player_frame_4
+
+    global cloud
+    #cloud
+
+    global obstacle_1
+    global obstacle_2
+    global obstacle_3
+    global obstacle_4
+    #obstacle_1
+    #obstacle_2
+    #obstacle_3
+    #obstacle_4
+
+    #ground
+    return player_init
+
+
